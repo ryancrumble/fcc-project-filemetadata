5a04c0967783a4670ec5747bef861230f26e662d
Ryan Crummy <ryan.crummy+github@twobulls.com>
2023-01-06 09:57:15 +1100

    feat: add file upload endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,47 @@
-import express from 'express';
 import cors from 'cors'
+import express from 'express';
+import multer from 'multer';
 
 import dotenv from 'dotenv'
 
 dotenv.config()
 
+const port = process.env.PORT || 4000;
+
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+// Multer config
+const upload = multer({ dest: "uploads/" });
+
+// Serve static assets
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+    const file = req.file
 
-const port = process.env.PORT || 4000;
+    if (!file) {
+        return res.json({error: "No file provided"})
+    }
+
+    const response = {
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.size
+    }
+
+    return res.json(response)
+})
+
+
 app.listen(port, function () {
     console.log(
         '\x1b[36m %s\x1b[0m',
